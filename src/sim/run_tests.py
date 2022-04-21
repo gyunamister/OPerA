f58cc6d0ccea0e9bccd7658d6be755d7a2077483
gyunamister <gyunamister@gmail.com>
2022-04-21 15:40:45 +0200

    initial commit to OPerA

--- src/sim/run_tests.py
+++
@@ -1,8 +0,0 @@
-from sim.sim_tests import *
-
-#run_test(*sequential_model(), 'sequential')
-#run_test(*sequential_model_modified(), 'sequential_modified')
-#run_test(*goto_model(), 'goto')
-#run_test(*another_one(), 'another')
-#run_test(*delay_model(), 'delay')
-run_test(*dst(), 'dst')
