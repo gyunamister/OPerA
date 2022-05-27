68d80e169ca75bc9cee83fcd709689b88ac2dbb8
gyunamister <gyunamister@gmail.com>
2022-05-27 09:57:03 +0200

    revise run_dtween.sh

--- src/backend/run_dtween.sh
+++ src/backend/run_dtween.sh
@@ -1,4 +1,4 @@
-export DTWEEN_PATH="/Users/gyunam/Documents/DigitalTwin/"
+# export DTWEEN_PATH="/Users/gyunam/Documents/DigitalTwin/"
 # export OCPA_PATH="/Users/gyunam/Documents/ocpa-core"
 dtween_path=$DTWEEN_PATH
 # ocpa_path=$OCPA_PATH
